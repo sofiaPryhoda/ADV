21d4e4dab7686191d06ab6f4a6cd66a3e8e4a80d
Sofia <77118283+sofiaPryhoda@users.noreply.github.com>
2022-01-04 23:06:41 +0200

    Update user-list.component.ts

--- frontend/src/app/user-list/user-list.component.ts
+++ frontend/src/app/user-list/user-list.component.ts
@@ -87,16 +87,15 @@ export class UserListComponent {
     });
   }
 
-  // @ts-ignore
-  open(content) {
+  open(content : any) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
-// @ts-ignore
-  private getDismissReason(reason): string {
+
+  private getDismissReason(reason : any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
